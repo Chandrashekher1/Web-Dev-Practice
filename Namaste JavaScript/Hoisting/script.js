18c5b9bd79e2644c26dc698b8def253cc6000bfc
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-07-13 12:43:55 +0530

    Basic of JavaScript

---
+++ Namaste JavaScript/Hoisting/script.js
@@ -0,0 +1,12 @@
+getName()
+console.log(x);   // It will return undefined 
+
+var x = 7
+// console.log(x);  // First print value of X then funtion data
+
+function getName() {
+    console.log("Namaste JavaScript");
+}
+
+// getName()
+// console.log(x);  // It will return 7
